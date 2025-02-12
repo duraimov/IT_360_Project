76f9e5ad58715c727862f14833604680c0c52250
duraimov <158231308+duraimov@users.noreply.github.com>
2025-02-12 14:06:59 -0600

    start

---
+++ Accepting_File.py
@@ -0,0 +1,37 @@
+import sys
+import os
+import re
+
+
+
+
+fileName = input("what is your file name?")
+
+readFile = open(fileName, "r")
+
+
+
+print(fileName.readline())
+
+
+
+
+
+# def readFile( fileName ):
+#     try:
+#       file = open( fileName, "r")
+#     except IOError:
+#       print ("There was an error reading file")
+#       sys.exit()
+#     file_text = file.read()
+#     file.close()
+#     return file_text
+
+# fileName = input("what is your file name?")
+
+# readFile(fileName)
+
+
+
+
+
