0a7040c88402b9831155f6bbc35d6afb0800f549
duraimov <158231308+duraimov@users.noreply.github.com>
2025-03-20 13:24:11 -0500

    add methods to daniel_testfile

--- src/scanner/daniel_testfile.py
+++ src/scanner/daniel_testfile.py
@@ -1,6 +1,26 @@
 # Daniel's file
 
+import virustotal_python
+import os
+
+
 def run_daniel():
 
     print('Daniel this is your main function. Whatever you want to do, just put it here.')
+
+
     return
+def isrtlext(file):
+    rtl_override_present = False
+    for char in file:
+        if ord(char) == 0x202E:
+            rtl_override_present = True
+            break
+    return rtl_override_present
+
+def toVirusTotal(file):
+    
+
+
+
+
