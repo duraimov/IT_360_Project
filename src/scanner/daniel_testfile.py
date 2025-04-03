762cf56d63553abc33042c0c9f27f85ff479901e
duraimov <158231308+duraimov@users.noreply.github.com>
2025-04-03 16:18:55 -0500

    Update daniel_testfile.py

--- src/scanner/daniel_testfile.py
+++ src/scanner/daniel_testfile.py
@@ -2,7 +2,9 @@
 
 import virustotal_python
 import os
+import requests
 
+api_key = "6acabfc35b112fd6c58b626055a8fe288e0f5571f03b099d44ffe3380b21a72b"
 
 def run_daniel():
 
@@ -11,16 +13,37 @@ def run_daniel():
 
     return
 
+   #checks for file extensions written right to left 
 def isrtlext(file):
     rtl_override_present = False
     for char in file:
-        if ord(char) == 0x202E:
+        if ord(char) == 0x202E: #if file name contains right to left unicode
             rtl_override_present = True
             break
     return rtl_override_present
 
 def toVirusTotal(file):
-    return    
+
+    
+    url = "https://www.virustotal.com/api/v3/files"
+    
+    # Open the file in binary mode
+    with open(file, 'rb') as new_file:
+        # Set the headers with your API key
+        headers = {"x-apikey": api_key}
+        
+        # Upload the file
+        response = requests.post(url, headers=headers, files={"file": file})
+        
+        # Check the response status
+        if response.status_code == 200:
+            print("File uploaded successfully!")
+            return response.json()  # Return the JSON response with the scan details
+        else:
+            print(f"Error: {response.status_code}")
+            print(response.text)
+            return None
+    
 
 
 
